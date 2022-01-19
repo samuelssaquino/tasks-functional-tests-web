78bbe0f08796c7aafde99b84069bd8e1572bde82
samuelssaquino <samuelssaquino@outlook.com>
2022-01-19 17:30:48 -0300

    remove test

--- src/test/java/br/ce/wcaquino/functionaltest/tasksTest.java
+++ src/test/java/br/ce/wcaquino/functionaltest/tasksTest.java
@@ -1,12 +1,13 @@
 package br.ce.wcaquino.functionaltest;
 
+import static org.junit.Assert.assertEquals;
+
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.time.Duration;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
-import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -38,7 +39,7 @@ public class tasksTest {
             driver.findElement(By.id("dueDate")).sendKeys(formattedString);
             driver.findElement(By.id("saveButton")).click();
             String msg = driver.findElement(By.id("message")).getText();
-            Assert.assertEquals("Success!", msg);
+            assertEquals("Success!", msg);
         } finally {
             driver.quit();
         }
@@ -54,7 +55,7 @@ public class tasksTest {
             driver.findElement(By.id("dueDate")).sendKeys("10/10/2022");
             driver.findElement(By.id("saveButton")).click();
             String msg = driver.findElement(By.id("message")).getText();
-            Assert.assertEquals("Fill the task description", msg);
+            assertEquals("Fill the task description", msg);
         } finally {
             driver.quit();
         }
@@ -70,7 +71,7 @@ public class tasksTest {
             driver.findElement(By.id("task")).sendKeys("Task via Selenium");
             driver.findElement(By.id("saveButton")).click();
             String msg = driver.findElement(By.id("message")).getText();
-            Assert.assertEquals("Fill the due date", msg);
+            assertEquals("Fill the due date", msg);
         } finally {
             driver.quit();
         }
@@ -87,7 +88,34 @@ public class tasksTest {
             driver.findElement(By.id("dueDate")).sendKeys("01/01/2021");
             driver.findElement(By.id("saveButton")).click();
             String msg = driver.findElement(By.id("message")).getText();
-            Assert.assertEquals("Due date must not be in past", msg);
+            assertEquals("Due date must not be in past", msg);
+        } finally {
+            driver.quit();
+        }
+    }
+
+    @Test
+    public void deveRemoverTarefaComSucesso() throws MalformedURLException{
+        
+        WebDriver driver = acessarAplicacao();
+
+        try {
+            //create task
+            driver.findElement(By.id("addTodo")).click();
+            driver.findElement(By.id("task")).sendKeys("Task via Selenium");            
+            LocalDate localDate = LocalDate.now();
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+            String formattedString = localDate.format(formatter);
+            driver.findElement(By.id("dueDate")).sendKeys(formattedString);
+            driver.findElement(By.id("saveButton")).click();
+            String msg = driver.findElement(By.id("message")).getText();
+            assertEquals("Success!", msg);
+
+           //remove task
+            driver.findElement(By.xpath("//a[@class='btn btn-outline-danger btn-sm']")).click();
+            msg = driver.findElement(By.id("message")).getText();
+            assertEquals("Success!", msg);
+
         } finally {
             driver.quit();
         }
